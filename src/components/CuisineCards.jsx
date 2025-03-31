55963eff136e47065075654de5b561148797de2e
Rishikesh <rishikeshpatil0605@gmail.com>
2025-03-31 21:54:40 +0530

    new images (replaced the old one's)

--- src/components/CuisineCards.jsx
+++ src/components/CuisineCards.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Main.css";
-import indian from "../assets/indian.jpeg";
-import japanese from "../assets/japanese.jpg";
-import korean from "../assets/korean.jpg";
-import thai from "../assets/thai.jpg";
-import american from "../assets/american.jpg";
-import french from "../assets/french.jpg";
 
+// Importing correct images
+import maharashtrian from "../assets/maharashtrian_cuisine.jpg";
+import gujarati from "../assets/gujrathi.jpg";
+import bengali from "../assets/bengali.jpg";
+import northIndian from "../assets/north-Indian.jpg";
+import northEasternIndian from "../assets/north-eastern-indian.jpg";
+import southIndian from "../assets/south-indian.jpg";
+
+// Updated cuisines array with correct image references
 const cuisines = [
-    { name: "MAHARASTRIAN", image: indian, path: "/maharashtrian" },
-    { name: "GUJRATHI", image: japanese, path: "/gujarati" },
-    { name: "BENGALI", image: korean, path: "/bengali" },
-    { name: "NORTH INDIAN", image: thai, path: "/north-indian" },
-    { name: "NORTH EASTERN INDIAN", image: american, path: "/north-eastern-indian" },
-    { name: "SOUTH INDIAN", image: french, path: "/south-indian" },
+    { name: "MAHARASHTRIAN", image: maharashtrian, path: "/maharashtrian" },
+    { name: "GUJARATI", image: gujarati, path: "/gujarati" },
+    { name: "BENGALI", image: bengali, path: "/bengali" },
+    { name: "NORTH INDIAN", image: northIndian, path: "/north-indian" },
+    { name: "NORTH EASTERN INDIAN", image: northEasternIndian, path: "/north-eastern-indian" },
+    { name: "SOUTH INDIAN", image: southIndian, path: "/south-indian" },
 ];
 
 const CuisineCards = () => {
-
     const navigate = useNavigate();
-
 
     return (
         <div>
             <div className="heading">
                 <h1>
-                    <span className="blockA">simple recipes made for</span>
+                    <span className="blockA">Simple recipes made for</span>
                     <span className="blockB">real, actual, everyday life</span>
                 </h1>
             </div>
@@ -41,7 +42,8 @@ const CuisineCards = () => {
                 </div>
             </section>
         </div>
-    )
-}
+    );
+};
 
-export default CuisineCards
+export default CuisineCards;
+
